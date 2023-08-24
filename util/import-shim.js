7cd02e088cf1729f906e70d291e51f9a4f06da8f
Zack <lorenx@163.com>
2023-08-24 11:35:30 +0800

    feat: 更新

--- util/import-shim.js
+++
@@ -1,3 +0,0 @@
-// needed for React 17+ JSX transform
-// https://bit.ly/3xNsL9f
-export * as React from 'react';
--- util/start-shim.ts
+++
@@ -1,5 +0,0 @@
-import Handler from '../.out/out';
-
-addEventListener('fetch', (event) => {
-  return event.respondWith(Handler(event.request));
-});
